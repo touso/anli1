efb158bcf677e423c59ba690f8e74b4d250b5fb6
gaoyizhe <gaoyizhe@qq.com>
2021-07-30 10:13:03 +0800

    git 1 commit

---
+++ src/zhe/yi/TestDemo.java
@@ -0,0 +1,9 @@
+package zhe.yi;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("test demo");
+        System.out.println("test demo1");
+        System.out.println("test demo2");
+    }
+}
